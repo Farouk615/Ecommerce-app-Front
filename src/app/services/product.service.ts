367fe246a16fa33840cf99d5e40b25c7baa55323
lenovo <faroukayadi1@gmail.com>
2021-11-27 23:43:23 +0100

    Adding product service and displaying a list of products

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8070/api/products';
+  constructor(private httpClient:HttpClient) { }
+  getProductsList(): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response=> response._embedded.products));
+  }
+}
+
+interface GetResponse{
+  _embedded:{
+    products:Product[];
+  }
+}
